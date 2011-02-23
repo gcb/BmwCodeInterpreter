0a84839c17f3611917e6b2bd370084427d55f0d0
Gabriel <descartavel1@gmail.com>
2011-02-22 19:16:15 -0800

    first commit

---
+++ src/xxx/bmwcodeinterpreter/BmwCodeInterpreterActivity.java
@@ -0,0 +1,15 @@
+package xxx.bmwcodeinterpreter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BmwCodeInterpreterActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
